2c65bd16ab799268e9209b894c7866f8b018530f
Clément TROMPIER <clem.trompier@gmail.com>
2023-01-24 14:56:26 +0100

    Ajout du modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,36 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+		private int id;
+		public int ID
+		{
+			get { return id; }
+			set {
+				if (value <= 0)
+					throw new ArgumentOutOfRangeException("L'id de la devise doit être strictement positif.");
+				id = value; 
+			}
+		}
+
+		private string? nomDevise;
+		public string? NomDevise
+		{
+			get { return nomDevise; }
+			set { nomDevise = value; }
+		}
+
+		private double taux;
+
+		public double Taux
+		{
+			get { return taux; }
+			set { taux = value; }
+		}
+
+		public Devise()
+		{
+
+		}
+	}
+}
