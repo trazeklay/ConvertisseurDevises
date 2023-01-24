2e3f96d4c0b2e2878182394904cd59f9bfbefc16
Clément TROMPIER <clem.trompier@gmail.com>
2023-01-24 15:22:48 +0100

    Création de la méthode 'GetAll()' dans le DevisesController.cs

---
+++ WSConvertisseur/Controllers/DevisesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WSConvertisseur.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WSConvertisseur.Controllers
+{
+    [Route("api/devises")]
+    [ApiController]
+    public class DevisesController : ControllerBase
+    {
+        private List<Devise> lesDevises;
+        public List<Devise> LesDevises
+        {
+            get { return lesDevises; }
+            set { lesDevises = value; }
+        }
+
+
+        // GET: api/<DevisesController>
+        [HttpGet]
+        public IEnumerable<Devise> GetAll()
+        {
+            return LesDevises;
+        }
+
+        // GET api/<DevisesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DevisesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DevisesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DevisesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        public DevisesController()
+        {
+            LesDevises = new List<Devise>()
+            {
+                new Devise(1, "Dollar", 1.08),
+                new Devise(2, "Franc Suisse", 1.07),
+                new Devise(3, "Yen", 120),
+            };
+        }
+    }
+}
