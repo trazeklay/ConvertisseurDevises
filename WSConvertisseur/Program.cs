351a1fc33784532cdc33de79e41b46c8a3a18962
Clément TROMPIER <clem.trompier@gmail.com>
2023-01-24 17:46:02 +0100

    Version à la fin du tp

--- WSConvertisseur/Program.cs
+++ WSConvertisseur/Program.cs
@@ -7,6 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen(doc =>
+{
+    var xmlFile = Path.ChangeExtension(typeof(Program).Assembly.Location, ".xml");
+    doc.IncludeXmlComments(xmlFile);
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
